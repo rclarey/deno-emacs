c375eae3e046d1487a45203511a6870e9dd5d2da
rclarey <r.e.clarey@gmail.com>
2020-05-20 14:38:18 -0400

    prep for melpa

--- deno-fmt.el
+++ deno-fmt.el
@@ -1,21 +1,39 @@
-;;; deno-fmt.el --- Minor mode for using deno fmt on save
-;;
-;; Copyright (C) 2020 Russell Clarey. All rights reserved. MIT license.
-;;
-;; Author: Russell Clarey <http://github/rclarey>
-;; Version: 0.1.0
-;; Keywords: deno format
-;; Homepage: https://github.com/russell/deno-emacs
-;;
+;;; deno-fmt.el --- Minor mode for using deno fmt on save -*- lexical-binding: t; -*-
+
+;;; Author: Russell Clarey <http://github/rclarey>
+;;; Package-Version: 0.1.0
+;;; URL: https://github.com/russell/deno-emacs
+;;; Package-Requires: ((emacs "24"))
+
+;; Copyright (c) 2020 Russell Clarey
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
 ;; This file is not part of GNU Emacs.
-;;
+
 ;;; Commentary:
 ;; Formats your TypeScript/JavaScript code with deno fmt on save
-;;
+
 ;;; Code:
 
 (defun deno-fmt ()
-  "Format buffer with `deno fmt'."
+  "Format the current buffer with `deno fmt'."
   (interactive)
   (if (not (executable-find "deno"))
       (error "deno executable not found. Visit \"https://deno.land/#installation\" for installation instructions")
@@ -45,8 +63,8 @@
   :lighter " deno-fmt"
   :global nil
   (if deno-fmt-mode
-      (add-hook 'before-save-hook 'deno-fmt nil 'local)
-    (remove-hook 'before-save-hook 'deno-fmt 'local)))
+      (add-hook 'before-save-hook #'deno-fmt nil 'local)
+    (remove-hook 'before-save-hook #'deno-fmt 'local)))
 
 (provide 'deno-fmt)
 ;;; deno-fmt.el ends here
